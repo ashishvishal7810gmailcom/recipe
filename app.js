dc23a9029edcc5c67d0733187dd2126e630d9b88
Ashish Vishal <ashishvishal7810@gmail.com>
2022-01-09 01:41:48 +0530

    heroku remove client nodemodules

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ app.use(express.static(path.join(__dirname, 'client','build')));
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
-app.all('*', (req,res,next) => {
-  if(req.secure) {
-    next();
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname+ ':'+app.get('secPort') + req.url);
-  }
-});
+// app.all('*', (req,res,next) => {
+//   if(req.secure) {
+//     next();
+//   }
+//   else {
+//     res.redirect(307, 'https://' + req.hostname+ ':'+app.get('secPort') + req.url);
+//   }
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
