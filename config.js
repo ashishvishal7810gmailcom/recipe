99de2ab5455aaca29474e7aa97feec88f3632429
Ashish Vishal <ashishvishal7810@gmail.com>
2022-01-09 00:04:29 +0530

    HEROKU

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/olx',
+    'mongoUrl' : 'mongodb+srv://phoenixwebhost:course01%40CP@cluster0.avp63.mongodb.net/phoenixwebhost?retryWrites=true&w=majority',
     'facebook': {
         clientId: 'xxx',
         clientSecret: 'xxx'
