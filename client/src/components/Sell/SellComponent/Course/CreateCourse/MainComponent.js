1a96e918ec7825747213cea391ced2cc3667fbcc
Ashish Vishal <ashishvishal7810@gmail.com>
2022-01-09 22:51:35 +0530

    image compress

--- client/src/components/Sell/SellComponent/Course/CreateCourse/MainComponent.js
+++ client/src/components/Sell/SellComponent/Course/CreateCourse/MainComponent.js
@@ -7,6 +7,7 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 // import { convertToRaw } from 'draft-js';
 // import { convertFromRaw } from 'draft-js';
 import { withRouter } from 'react-router-dom';
+import Compressor from 'compressorjs';
 
 const options = [
     { value: 'CP', label: 'CP' },
@@ -79,7 +80,18 @@ class CreateCourse extends Component {
     // On file select (from the pop up)
 	onFileChange = event => {
         // Update the state
-        this.setState({ selectedFile: event.target.files[0] });
+        // this.setState({ selectedFile: event.target.files[0] });
+        const image = event.target.files[0];
+        new Compressor(image, {
+        quality: 0.8, // 0.6 can also be used, but its not recommended to go below.
+        success: (compressedResult) => {
+            // compressedResult has the compressed file.
+            // Use the compressed file to upload the images to your server.  
+            this.setState({ selectedFile: compressedResult });      
+            // setCompressedFile(compressedResult)
+        },
+        });
+        
 	};
 
     onEditorStateChange = (editorState) => {
